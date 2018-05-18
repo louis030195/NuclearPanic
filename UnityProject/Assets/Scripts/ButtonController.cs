7beca9f97e2d553d7293b7f1601f5b1a8edd270f
Sonlight <louis.beaumont@gmail.com>
2018-05-18 11:54:55 +0200

    Add buttons

---
+++ UnityProject/Assets/Scripts/ButtonController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(Collider))]
+public class ButtonController : MonoBehaviour
+{
+    private Vector3 startingPosition;
+    private Renderer myRenderer;
+    private TextMesh textMesh;
+
+    public Material inactiveMaterial;
+    public Material gazedAtMaterial;
+
+    void Start()
+    {
+        myRenderer = GetComponent<Renderer>();
+        textMesh = GetComponent<TextMesh>();
+
+        SetGazedAt(false);
+
+    }
+
+
+
+    public void SetGazedAt(bool gazedAt)
+    {
+        if (inactiveMaterial != null && gazedAtMaterial != null)
+        {
+            myRenderer.material = gazedAt ? gazedAtMaterial : inactiveMaterial;
+            return;
+        }
+    }
+
+
+
+
+
+}
+
